cc9bac4eb41896b88602fe09568ec1e6a93775ce
veekay17 <vijay.vuex@gmail.com>
2022-06-02 23:18:54 +0530

    searchBox added to the app

--- src/App.js
+++ src/App.js
@@ -1,38 +1,66 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
+import SearchBox from "./components/search-box";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-    };
-  }
-  componentDidMount() {
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       monsters: [],
+//     };
+//     console.log("1");
+//   }
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((users) =>
+//         this.setState(
+//           () => {
+//             return { monsters: users };
+//           },
+//           () => {
+//             console.log(this.state.monsters[0]);
+//           }
+//         )
+//       );
+//     console.log("3");
+//   }
+
+//   render() {
+//     console.log("render");
+
+//     return (
+//       <div className="App">
+//         {this.state.monsters.map((monster, i) => {
+//           return <h1 key={i}>{monster.name}</h1>;
+//         })}
+//         <button>Change Name</button>
+//       </div>
+//     );
+//   }
+// }
+const App = () => {
+  const [monsters, setMonster] = useState("");
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) =>
-        this.setState(
-          () => {
-            return { monsters: users };
-          },
-          () => {
-            console.log(this.state);
-          }
-        )
-      );
-  }
+      .then((res) => res.json())
+      .then((users) => setMonster(users));
+  }, []);
+  // console.log(monsters);
 
-  render() {
-    return (
-      <div className="App">
-        {this.state.monsters.map((monster, i) => {
-          return <h1 key={i}>{monster.name}</h1>;
-        })}
-        <button>Change Name</button>
-      </div>
-    );
-  }
-}
-
+  const onSearchChange = (e) => {
+    const searchFieldStr = e.target.value.toLocaleLowerCase();
+    console.log(searchFieldStr);
+  };
+  return (
+    <div className="App">
+      <h1 className="app-title">Monster Rolodex</h1>
+      <SearchBox
+        className="search-box"
+        onChangeHandler={onSearchChange}
+        placeholder="search monsters"
+      />
+    </div>
+  );
+};
 export default App;
